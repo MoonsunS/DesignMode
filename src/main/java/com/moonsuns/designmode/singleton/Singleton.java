b5556f2ad9c27a012932a662b74b95b35a2fa21a
MoonsunS <yueyangsong@foxmail.com>
2018-09-02 23:08:44 +0800

    singleton

---
+++ src/main/java/com/moonsuns/designmode/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package com.moonsuns.designmode.singleton;
+
+public class Singleton
+{
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton getInstance()
+    {
+        if (uniqueInstance == null)
+        {
+            synchronized (Singleton.class)
+            {
+                if (uniqueInstance == null)
+                {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
